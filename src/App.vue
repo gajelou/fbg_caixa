9002be777513e6495f7fa444049c364ec36ebe45
camargo e barros <gabrieljelourenco@gmail.com>
2026-05-26 15:25:16 -0300

    server fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 
-const API_URL = "https://acknowledged-andrew-file-exec.trycloudflare.com";
+const API_URL = "https://county-common-latitude-system.trycloudflare.com";
 
 const agio = ref<number>(30);
 const mostrarPrecos = ref(true);
