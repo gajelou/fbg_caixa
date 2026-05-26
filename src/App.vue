d065ae44962d7308a395a9fda63584b39fab4806
camargo e barros <gabrieljelourenco@gmail.com>
2026-05-26 11:49:37 -0300

    atualizando servidor

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 
-const API_URL = "https://scraper-ceb.onrender.com";
+const API_URL = "https://acknowledged-andrew-file-exec.trycloudflare.com";
 
 const agio = ref<number>(30);
 const mostrarPrecos = ref(true);
